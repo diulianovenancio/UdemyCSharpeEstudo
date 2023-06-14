3b8186b4995ce9ed49d891642937a029be6c2ea7
Diuliano Venâncio <diuliano1@gmail.com>
2023-06-14 10:10:27 -0300

    Exercício Aula 18

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PrimeiroProjeto
 {
@@ -6,7 +7,25 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Exercicio Aula 18
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2:F2}");
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 }
